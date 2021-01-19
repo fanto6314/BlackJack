3d64fd56e5f7704dcd47e6a959f5de53f29f945b
Antonio <62208053+fanto6314@users.noreply.github.com>
2021-01-19 15:11:29 +0100

    Update Cards.java

--- Cards.java
+++ Cards.java
@@ -1,4 +1,4 @@
-package test.dev.anto6314.blackJack2;
+package dev.anto6314.blackJack;
 
 public class Cards {
 
@@ -7,31 +7,40 @@ public class Cards {
     private String[] numString = {"Uno", "Due", "Tre", "Quattro", "Cinque", "Sei", "Sette", "Otto", "Nove", "Dieci", "Fante", "Regina", "Re"};
 
     /**
-     * Cards constructor
-     * @param sType
-     * @param sNum
+     * Costruttore della classe Cards
+     *
+     * @param stype
+     * @param snum
      */
-
-    public Cards(Suits stype, int snum){
+    public Cards(Suits stype, int snum) {
 
         this.cardSuit = stype;
 
-        if(snum >=1 && snum <= 13)
-            this.cardNum  = snum;
-        else{
+        if (snum >= 1 && snum <= 13)
+            this.cardNum = snum;
+        else {
 
-            System.err.println(snum+" is not a valid card number\n");
+            System.err.println(snum + " is not a valid card number\n");
             System.exit(1);
         }
     }
 
-    public int getCardNumber(){
+    /**
+     * Questo metodo ritorna il numero della carda
+     *
+     * @return
+     */
+    public int getCardNumber() {
 
         return this.cardNum;
     }
 
-    public String toString(){
-
-        return this.numString[this.cardNum - 1]+" di "+this.cardSuit.toString();
+    /**
+     * Questo metodo ritorna il numero della carta + il tipo di carta
+     *
+     * @return
+     */
+    public String toString() {
+        return this.numString[this.cardNum - 1] + " di " + this.cardSuit.toString();
     }
 }
