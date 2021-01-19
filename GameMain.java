955b209f6d74b87cefb4f7c42c4d80d4b1188ad6
Antonio <62208053+fanto6314@users.noreply.github.com>
2021-01-19 15:12:55 +0100

    Update GameMain.java

--- GameMain.java
+++ GameMain.java
@@ -1,22 +1,22 @@
-package dev.anto6314;
+package dev.anto6314.blackJack;
 
 import java.util.Scanner;
 
+/**
+ * Main
+ */
 public class GameMain {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         String playerName;
 
-        System.out.println("\t\t\t\t-----------------------------");
-        System.out.println("\t\t\t\t|   BLACKJACK by anto6314   |");
-        System.out.println("\t\t\t\t-----------------------------\n");
-
+        System.out.println("\t\t\t\t-----------------");
+        System.out.println("\t\t\t\t|   BLACKJACK   |");
+        System.out.println("\t\t\t\t-----------------");
         System.out.print("Inserisci il tuo nome: ");
-        playerName = scanner.nextLine();
-
+        playerName = s.nextLine();
         GameCore GameMain = new GameCore(playerName);
 
-        scanner.close();
+        s.close();
     }
 }
