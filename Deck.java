892c889f295ac78404044fedc954a5e6aa1d868b
Antonio <62208053+fanto6314@users.noreply.github.com>
2021-01-19 15:11:53 +0100

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,72 +1,52 @@
-package test.dev.anto6314.blackJack2;
+package dev.anto6314.blackJack;
 
 import java.util.Random;
 
 public class Deck {
 
-    // cards in the deck --- arraylist or linkedlist could also be used
     private Cards[] cardsInDeck;
     private int numOfCardsInDeck;
     private int onePack = 52;
 
     /**
-     * constructor for default shuffled deck consisting of 1 pack i.e 52 cards
+     * costruttore per il mescolamento di base del mazzo (considerando un pacco da 52 carte)
      */
-
     public Deck(){
-
         this(1, true);
     }
 
     /**
-     *
-     * Deck constructor to define number of cards in a Deck && whether it should be shuffled or not
-     *
+     * Costruttore che definisce il numero di carte in un Deck, e se deve essere mescolato oppure no
      * @param numPacks
      * @param shuffle
      */
     public Deck(int numPacks, boolean shuffle){
-
         this.numOfCardsInDeck = numPacks*this.onePack;
         this.cardsInDeck = new Cards[this.numOfCardsInDeck];
 
         int c = 0;
 
-        // for each deck
         for(int d=0;d<numPacks;d++){
-
-            // for each suit
             for(int s=0; s<4;s++){
-
-                // for each number
                 for(int n=1;n<=13;n++){
-
                     this.cardsInDeck[c] = new Cards(Suits.values()[s], n);
                     c++;
                 }
             }
         }
 
-        //shuffle
         if(shuffle){
-
             this.shuffleDeck();
         }
     }
-
-
-    // custom shuffle function--if <list> was used, it has a shuffle method build in
+    /**
+     * Metodo che mescola le carte tra di loro
+     */
     public void shuffleDeck(){
-
         Random rng = new Random();
-
         Cards temp;
-
-        // swapping
-
         int j;
         for(int i=0; i<this.numOfCardsInDeck;i++){
-
             j = rng.nextInt(this.numOfCardsInDeck);
 
             temp = this.cardsInDeck[i];
@@ -75,16 +55,14 @@ public class Deck {
         }
     }
 
-
-    // dealing a card from deck
+    /**
+     * Metodo che distribuisce le carte
+     * @return
+     */
     public Cards dealingNextCard(){
-
         Cards topCard = this.cardsInDeck[0];
 
-        // remove this card -- if arraylist was used, remove method is build in
-
         for(int c =1; c<this.numOfCardsInDeck;c++){
-
             this.cardsInDeck[c-1] = this.cardsInDeck[c];
         }
 
@@ -94,10 +72,12 @@ public class Deck {
         return topCard;
     }
 
+    /**
+     * Metodo che stampa le carte
+     * @param num
+     */
     public void printDeckCards(int num){
-
         for(int c=0; c<num; c++){
-
             System.out.printf("% 3d/%d %s\n", c+1, this.numOfCardsInDeck, this.cardsInDeck[c].toString());
         }
 
